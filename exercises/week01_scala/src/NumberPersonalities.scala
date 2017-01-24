78090c2fcd74e6872630192c20ea795348af61f4
Chris Kimberley <chris@chriskimberley.com>
2017-01-24 01:29:19 +0000

    SDP Scala Primer Ex03 - just starting

---
+++ exercises/week01_scala/src/NumberPersonalities.scala
@@ -0,0 +1,13 @@
+/**
+  * @author Chris Kimberley
+  */
+object NumberPersonalities {
+  val limit = 100
+
+  def main(args: Array[String]): Unit = {
+    for (number <- 1 to limit) {
+      //println(number)
+    }
+  }
+
+}

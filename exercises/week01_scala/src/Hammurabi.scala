3dfbeb7a4a49049fb800d96dff09415acfb6b4b9
Chris Kimberley <ckimbe02@dcs.bbk.ac.uk>
2017-01-24 20:06:05 +0000

    ...

--- exercises/week01_scala/src/Hammurabi.scala
+++ exercises/week01_scala/src/Hammurabi.scala
@@ -56,9 +56,9 @@ object Hammurabi extends App {
   }
 
   def printIntroductoryMessage = println(
-    """
-      |Congratulations, you are the newest ruler of ancient Samaria, elected
-      |for a ten year term of office. Your duties are to dispense food, direct
+    """of ancient Samaria, elected
+      |      |for a ten year term of office. Your duties
+      |Congratulations, you are the newest ruler are to dispense food, direct
       |farming, and buy and sell land as needed to support your people. Watch
       |out for rat infestations and the plague! Grain is the general currency,
       |measured in bushels. The following will help you in your decisions:

8c8f63d3f84587f6cf6341cce6163fb3c360ef90
Chris Kimberley <chris@chriskimberley.com>
2017-02-03 17:22:00 +0000

    S.O.L.I.D./alarmsystem exercise - implemented FireSensor methods

--- exercises/week02/alarmsystem-scala/src/FireSensor.scala
+++ exercises/week02/alarmsystem-scala/src/FireSensor.scala
@@ -1,26 +1,27 @@
 import scala.util.Random
 
-class FireSensor extends Sensor {
+class FireSensor(location: String) extends Sensor {
   val alarmFrequencyPercentage = 5
   val r = new Random()
   val batteryDrainBetweenPollsPercentage = 10
   var batteryPercentage = 100
   var triggered: Boolean = false
 
-  override def isTriggered: Boolean = {
+
+  override def isTriggered = {
     triggered =  if (batteryPercentage > 0) r.nextInt(100) < alarmFrequencyPercentage else false
     batteryDrain
     triggered
   }
 
-  def batteryDrain: Unit = {
+  def batteryDrain {
     batteryPercentage = if (batteryPercentage > batteryDrainBetweenPollsPercentage)
       batteryPercentage - batteryDrainBetweenPollsPercentage else 0
   }
 
-  override def getLocation: String = null
+  override def getLocation = location
 
-  override def getSensorType: String = null
+  override def getSensorType = "Fire"
 
-  override def getBatteryPercentage: Double = -1
+  override def getBatteryPercentage = batteryPercentage
 }

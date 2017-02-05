a620b1d7b023ddda69e0d6608724b42d999310b3
Chris Kimberley <chris@chriskimberley.com>
2017-02-05 17:58:30 +0000

    S.O.L.I.D./alarmsystem - deleting some print statements used for testing

--- exercises/week02/alarmsystem-scala/src/HazardSensor.scala
+++ exercises/week02/alarmsystem-scala/src/HazardSensor.scala
@@ -8,9 +8,6 @@ trait HazardSensor extends Sensor {
 
   def isTriggered(batteryPercentage: Int, alarmFrequency: Int) = {
     var randomInt = r.nextInt(100)
-    println("alarmFrequency = " + alarmFrequency + ", " +
-      "batteryPercentage = " + batteryPercentage + ", " + "randomInt = " + randomInt)
-    println(batteryPercentage)
     if (batteryPercentage > 0) randomInt < alarmFrequency else false
   }
 

0fd05955a216a8a9b9ae9290aba711d7562ec04d
Chris Kimberley <chris@chriskimberley.com>
2017-02-05 02:13:48 +0000

    S.O.L.I.D./alarmsystem - up to Ex.8: SensorCategory enum introduced for using in getSensorCategory method (added to Sensor trait)

--- exercises/week02/alarmsystem-scala/src/ControlUnit.scala
+++ exercises/week02/alarmsystem-scala/src/ControlUnit.scala
@@ -3,9 +3,6 @@ import scala.collection.mutable
 class ControlUnit(sensors: mutable.Buffer[Sensor]) {
 
   def pollSensors {
-    //val sensors = new ListBuffer[Sensor]()
-    //sensors += new FireSensor()
-    //sensors += new SmokeSensor()
     for (sensor <- sensors) {
       if (sensor.isTriggered) {
         System.out.println("A " + sensor.getSensorType + " sensor was triggered at " + sensor.getLocation)

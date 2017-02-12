c823c9a8f5b3926f43363afa16266a0d6b5179c6
Chris Kimberley <chris@chriskimberley.com>
2017-02-12 00:50:49 +0000

    week05  - 1(b) Counter augmented to allow user to optionally pass an Int parameter to int & dec...default to 1
     - 1(c) Reimplemented Counter as a case class, initialising count to default value 0

--- exercises/week05/src/main/scala/Counter.scala
+++ exercises/week05/src/main/scala/Counter.scala
@@ -8,7 +8,7 @@ class Counter(val count: Int) {
 }
 */
 
-case class Counter(count: Int = 1) {
+case class Counter(count: Int = 0) {
   def inc(value: Int = 1) = new Counter(count + 1)
   def dec(value: Int = 1) = new Counter(count - 1)
 }

8d69dfe6db47f1e05eaf02477f0afc4115180f90
Chris Kimberley <chris@chriskimberley.com>
2017-02-12 00:50:26 +0000

    week05  - 1(b) Counter augmented to allow user to optionally pass an Int parameter to int & dec...default to 1
     - 1(c) Reimplemented Counter as a case class, initialising count to default value 0

--- exercises/week05/src/main/scala/Counter.scala
+++ exercises/week05/src/main/scala/Counter.scala
@@ -1,7 +1,14 @@
 /**
   * @author Chris Kimberley
   */
+/*
 class Counter(val count: Int) {
-  def inc = new Counter(count + 1)
-  def dec = new Counter(count - 1)
+  def inc(value: Int = 1) = new Counter(count + 1)
+  def dec(value: Int = 1) = new Counter(count - 1)
+}
+*/
+
+case class Counter(count: Int = 1) {
+  def inc(value: Int = 1) = new Counter(count + 1)
+  def dec(value: Int = 1) = new Counter(count - 1)
 }
--- exercises/week05/src/test/scala/ClassesTestSuite.scala
+++ exercises/week05/src/test/scala/ClassesTestSuite.scala
@@ -9,14 +9,15 @@ class ClassesTestSuite extends FunSuite {
   }
 
   test("inc method increments counter and returns a new Counter") {
-    assert(new Counter(19).inc.count == 20)
+    assert(new Counter(19).inc().count == 20)
   }
 
   test("dec method decrements counter and returns a new Counter") {
-    assert(new Counter(29).dec.count == 28)
+    assert(new Counter(29).dec().count == 28)
   }
 
   test("Sequence of inc & dec calls returns new Counter with correct count") {
-    assert(new Counter(10).inc.dec.inc.inc.count == 12)
+    assert(new Counter(10).inc().dec().inc().inc().count == 12)
   }
+
 }

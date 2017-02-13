0a91aa421f113d46125dc71e00b7730415f98a3a
Chris Kimberley <chris@chriskimberley.com>
2017-02-13 15:07:04 +0000

    Week05 - Ex.3 - Completed. Director & Film classes converted to case classes. Duplicate apply methods commented out. Retested

--- exercises/week05/src/main/scala/Director.scala
+++ exercises/week05/src/main/scala/Director.scala
@@ -1,17 +1,20 @@
 /**
   * @author Chris Kimberley
   */
-class Director(firstName: String, lastName: String, val yearOfBirth: Int) extends
+/*class Director(firstName: String, lastName: String, val yearOfBirth: Int) extends
   Person(firstName: String, lastName: String) {
 
+  def name = s"$firstName $lastName"
+}*/
+case class Director(firstName: String, lastName: String, yearOfBirth: Int) {
   def name = s"$firstName $lastName"
 }
 
 object Director {
 
-  def apply(firstName: String, lastName: String, yearOfBirth: Int) = {
+  /*def apply(firstName: String, lastName: String, yearOfBirth: Int) = {
     new Director(firstName, lastName, yearOfBirth)
-  }
+  }*/
 
   def older(director1: Director, director2: Director) = {
     if (director1.yearOfBirth < director2.yearOfBirth) director1 else director2
--- exercises/week05/src/main/scala/Film.scala
+++ exercises/week05/src/main/scala/Film.scala
@@ -1,7 +1,7 @@
 /**
   * @author Chris Kimberley
   */
-class Film(val name: String, val yearOfRelease: Int, val imdbRating: Double, val director: Director) {
+/*class Film(val name: String, val yearOfRelease: Int, val imdbRating: Double, val director: Director) {
 
   def directorsAge = yearOfRelease - director.yearOfBirth
 
@@ -22,12 +22,35 @@ class Film(val name: String, val yearOfRelease: Int, val imdbRating: Double, val
       case _ => false
     }
   }
+}*/
+
+case class Film(name: String, yearOfRelease: Int, imdbRating: Double, director: Director) {
+
+  def directorsAge = yearOfRelease - director.yearOfBirth
+
+  def isDirectedBy(director: Director) = this.director == director
+
+  def copy(name: String = this.name, yearOfRelease: Int = this.yearOfRelease,
+           imdbRating: Double = this.imdbRating, director: Director = this.director) = {
+    new Film(name, yearOfRelease, imdbRating, director)}
+
+  override def toString: String = {
+    s"$name, $yearOfRelease, $imdbRating, ${director.name}"
+  }
+
+  override def equals(that: Any) = {
+    that match {
+      case x: Film => x.name == name && x.yearOfRelease == yearOfRelease &&
+        x.imdbRating == imdbRating && x.director == director
+      case _ => false
+    }
+  }
 }
 
 object Film {
-  def apply(name: String, yearOfRelease: Int, imdbRating: Double, director: Director) = {
+  /*def apply(name: String, yearOfRelease: Int, imdbRating: Double, director: Director) = {
     new Film(name, yearOfRelease, imdbRating, director)
-  }
+  }*/
 
   def highestRating(film1: Film, film2: Film) = {
     if (film1.imdbRating > film2.imdbRating) film1 else film2

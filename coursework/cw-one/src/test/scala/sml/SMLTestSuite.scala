e8bde6f87e3258d83e98b20ce8e17c8ac430a87c
Chris Kimberley <chris@chriskimberley.com>
2017-03-02 01:01:14 +0000

    cw-one: println statement removed from AddInstruction test

--- coursework/cw-one/src/test/scala/sml/SMLTestSuite.scala
+++ coursework/cw-one/src/test/scala/sml/SMLTestSuite.scala
@@ -29,13 +29,12 @@ class SMLTestSuite extends FunSpec with BeforeAndAfter {
       }
     }
 
-    it ("should return the correct toString value") {
+    it ("toString should return 'label: opcode op1 op2 to result +\\n'") {
       var integer1 = random.nextInt(1000000)
       var integer2 = random.nextInt(1000000)
       val storeInteger1 = LinInstruction("A0", 0, integer1)
       val storeInteger2 = LinInstruction("A1", 1, integer2)
       val add2Integers = AddInstruction("A2", 2, 0, 1)
-      // label + ": " + opcode + " " + op1 + " + " + op2 + " to " + result + "\n
       assert(add2Integers.toString() === "A2: add 0 + 1 to 2\n")
     }
   }

ec2c9ec089f9f83bb198922347137c454ada991a
crkimberley <ckimberley@gmail.com>
2017-04-21 14:42:14 +0100

    cw2: VirtualMachineParser - starting implementation, using composition of the other 2 parsers - programParser and byteCodeParser

---
+++ coursework/cw-two/src/main/scala/vm/VirtualMachineParserImpl.scala
@@ -0,0 +1,35 @@
+package vm
+import bc.{ByteCode, ByteCodeParserImpl}
+import vendor.ProgramParserImpl
+
+/**
+  * @author Chris Kimberley
+  */
+class VirtualMachineParserImpl extends VirtualMachineParser {
+  val programParser = new ProgramParserImpl
+  val byteCodeParser = new ByteCodeParserImpl
+
+  /**
+    * Returns a vector of [[bc.ByteCode]].
+    *
+    * This method parses a file into a vector of bytecode objects.
+    * Note, this method should throw a [[bc.InvalidBytecodeException]]
+    * if it fails to parse a program file correctly.
+    *
+    * @param file the file containing a program
+    * @return a vector of bytecodes
+    */
+  override def parse(file: String): Vector[ByteCode] = ???
+
+  /**
+    * Returns a vector of [[bc.ByteCode]].
+    *
+    * This method parses a string into a vector of bytecode objects.
+    * Note, this method should throw a [[bc.InvalidBytecodeException]]
+    * if it fails to parse a program string correctly.
+    *
+    * @param str a string containing a program
+    * @return a vector of bytecodes
+    */
+  override def parseString(str: String): Vector[ByteCode] = ???
+}
